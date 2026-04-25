d94ba315f821f2b7bbdd553c44a117f70ecdc4a2
Tobias Struckmeier <github@struckmeier.name>
2026-04-25 21:11:14 +0200

    Add AI generated texture data

---
+++ src/aiTextureData.js
@@ -0,0 +1,7 @@
+export const aiTextureData = {
+  concrete: '/factory-builder-3d/textures/concrete.jpg',
+  wall: '/factory-builder-3d/textures/wall.jpg',
+  metal: '/factory-builder-3d/textures/metal.jpg',
+  belt: '/factory-builder-3d/textures/belt.jpg',
+  hazard: '/factory-builder-3d/textures/hazard.jpg'
+};
